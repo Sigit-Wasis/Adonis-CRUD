f6815052975975c23238fd0a5623cb0052e119c8
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-02-07 09:31:17 +0700

    create post

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -3,10 +3,28 @@ const Post = use('App/Models/Post')
 
 
 class PostController {
+	// view read data post
 	async index({ request, response, view }) {
   		const posts = await Post.all()
 
   		return view.render('posts.index', { posts: posts.rows })
+	}
+
+	// view create post
+	create({ request, response, view }) {
+		return view.render('posts.create')
+	}
+
+	// insert data post
+	async store({ request, response, view, session }) {
+		const post = new Post()
+
+		post.title = request.input('title')
+		post.content = request.input('content')
+		await post.save()
+
+		session.flash({ notification: 'Data Berhasil Disimpan' })
+		return response.route('posts.index')
 	}
 }
 
