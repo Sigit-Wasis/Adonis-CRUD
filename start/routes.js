ddaa1a3880eac1421d5fcfb69f55dd05cf582a18
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-03-08 18:19:15 +0700

    add kajian

--- start/routes.js
+++ start/routes.js
@@ -17,6 +17,8 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+// ADD POST
 Route.get('/posts', 'PostController.index').as('posts.index')
 Route.get('/posts/create', 'PostController.create').as('posts.create')
 Route.post('/posts/store', 'PostController.store').as('posts.store')
@@ -24,10 +26,18 @@ Route.get('/posts/edit/:id', 'PostController.edit').as('posts.edit')
 Route.post('/posts/update/:id', 'PostController.update').as('posts.update')
 Route.get('/posts/delete/:id', 'PostController.delete').as('posts.delete')
 
+// ADD KAJIAN
+Route.get('/kajians', 'PostController.index').as('kajians.index')
+Route.get('/kajians/create', 'PostController.create').as('kajians.create')
+Route.post('/kajians/store', 'PostController.store').as('kajians.store')
+Route.get('/kajians/edit/:id', 'PostController.edit').as('kajians.edit')
+Route.post('/kajians/update/:id', 'PostController.update').as('kajians.update')
+Route.get('/kajians/delete/:id', 'PostController.delete').as('kajians.delete')
+
 Route.get('login', 'Auth/LoginController.index').as('login.index').middleware(['RedirectIfAuthenticated'])
 Route.post('login', 'Auth/LoginController.check').as('login.check').middleware(['RedirectIfAuthenticated'])
 Route.get('logout', 'Auth/LoginController.logout').as('logout').middleware(['Authenticate'])
 Route.get('/register', 'Auth/RegisterController.index').as('register.index').middleware(['RedirectIfAuthenticated'])
 Route.post('/register', 'Auth/RegisterController.store').as('register.store').middleware(['RedirectIfAuthenticated'])
 
-Route.get('/dashboard', 'DashboardController.index').as('dashboard').middleware(['Authenticate'])
+Route.get('/dashboard', 'DashboardController.index').as('dashboard')
